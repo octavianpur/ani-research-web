abbe03dc37500085a225438d2f10ef2d4a7dcd77
DorneeanWebApps <office@dorneean.ro>
2022-01-25 00:04:20 +0200

    update user added

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -23,6 +23,49 @@ const userService = {
     }
     return response;
   },
+
+  updateUserAsAdmin: async(tokenStatus: {
+    token: string;
+    active: boolean;
+  }, userData: User): Promise<User> => {
+    let response: any;
+    if(tokenStatus.active){
+      const config = {
+        headers: { Authorization: `Bearer ${tokenStatus.token}` },
+      };
+      try {
+        response = await axios.put(`${API_BASE_URL}/users/${userData.id}`,userData , config);
+        const resData = await response.data;
+        return resData;
+      } catch (error) {
+        response = error;
+        console.log(error);
+      }
+    }
+    return response;
+  },
+
+  updateCurrentUser: async(tokenStatus: {
+    token: string;
+    active: boolean;
+  }, userData: User): Promise<User> => {
+    let response: any;
+    if(tokenStatus.active){
+      const config = {
+        headers: { Authorization: `Bearer ${tokenStatus.token}` },
+      };
+      try {
+        response = await axios.put(`${API_BASE_URL}/users`,userData , config);
+        const resData = await response.data;
+        return resData;
+      } catch (error) {
+        response = error;
+        console.log(error);
+      }
+    }
+    return response;
+  }
+
 };
 
 export default userService;
