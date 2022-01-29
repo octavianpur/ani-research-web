d90e5f221624f3e7ae4f2c404339a05ba13722f4
octavianpur <octavian.p@gmail.com>
2022-01-29 12:03:42 +0200

    Update apiLinks.ts

--- src/resources/apiLinks.ts
+++ src/resources/apiLinks.ts
@@ -1,8 +1,8 @@
 
 // export const API_BASE_URL = 'http://localhost:3300/api';
-export const API_BASE_URL = 'https://dev.dorneean.ro/api';
-export const WEB_BASE_URL = 'http://localhost:7000';
+//export const API_BASE_URL = 'https://dev.dorneean.ro/api';
+//export const WEB_BASE_URL = 'http://localhost:7000';
 
 
-// export const API_BASE_URL = 'https://api-research.azurewebsites.net/api';
-// export const WEB_BASE_URL = 'https://salmon-meadow-00f8fb303.azurestaticapps.net';
+export const API_BASE_URL = 'https://api-research.azurewebsites.net/api';
+export const WEB_BASE_URL = 'https://nice-ground-0ff49b203.1.azurestaticapps.net/';
