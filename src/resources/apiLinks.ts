d9919b9968d6340f2c7816b86757c5290ee0bf4b
octavianpur <octavian.p@gmail.com>
2022-01-30 16:12:28 +0200

    Update apiLinks.ts

--- src/resources/apiLinks.ts
+++ src/resources/apiLinks.ts
@@ -4,5 +4,5 @@
 //export const WEB_BASE_URL = 'http://localhost:7000';
 
 
-export const API_BASE_URL = 'https://api-research.azurewebsites.net/api';
+export const API_BASE_URL = 'https://nice-ground-0ff49b203.1.azurestaticapps.net/api';
 export const WEB_BASE_URL = 'https://nice-ground-0ff49b203.1.azurestaticapps.net';
